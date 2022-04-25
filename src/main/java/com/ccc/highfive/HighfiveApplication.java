e5d919f02dab3daba961114c91de1e84d80f9d55
angbuilgu <sksmsdlscks@gmail.com>
2022-04-25 17:21:52 +0900

    first commit
    first commit

---
+++ src/main/java/com/ccc/highfive/HighfiveApplication.java
@@ -0,0 +1,13 @@
+package com.ccc.highfive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HighfiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HighfiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ccc/highfive/HighfiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ccc.highfive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HighfiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
