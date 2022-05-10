32d84ba43e5f71fff8eeca9d6c711c60eba957b7
kyun <88278485+kkyuny@users.noreply.github.com>
2022-05-11 02:43:41 +0900

    한균_notice-dao_ver1

---
+++ src/main/java/com/ccc/dao/NoticeDAO.java
@@ -0,0 +1,23 @@
+package com.ccc.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.ccc.dto.NoticeDTO;
+
+@Repository("NoticeDAO")
+public class NoticeDAO {
+	@Autowired
+	SqlSession session;
+	
+	public List<NoticeDTO> selectAllNotice() throws Exception{
+		return session.selectList("com.config.NoticeMapper.selectAllNotice");
+	}
+	
+	public NoticeDTO selectNoticeContents(int num) throws Exception{
+		return session.selectOne("com.config.NoticeMapper.selectNoticeContents");
+	}
+}
