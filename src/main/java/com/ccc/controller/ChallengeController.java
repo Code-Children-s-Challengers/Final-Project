d77c7f8ed7e39e67e6c7d936004c6e53fbf0e103
angbuilgu <sksmsdlscks@gmail.com>
2022-05-10 20:26:15 +0900

    무진_challenge페이지_ver1.6_challenge등록

--- src/main/java/com/ccc/controller/ChallengeController.java
+++ src/main/java/com/ccc/controller/ChallengeController.java
@@ -1,6 +1,10 @@
 package com.ccc.controller;
 
+import java.io.File;
+import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Date;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
@@ -19,6 +23,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.ccc.dto.ChallengeDTO;
 import com.ccc.service.ChallengeService;
@@ -101,6 +106,42 @@ public class ChallengeController {
 	public String makeChallengePopup() throws Exception{
 		
 		return "challenge/makeChallengePopup";
+	}
+	
+	@RequestMapping(value="/makeChallenge", method = RequestMethod.POST)
+	@ResponseBody
+	public String makeChallenge(@RequestParam MultipartFile photo, @RequestParam String name, @RequestParam String category, @RequestParam Date start_date, @RequestParam Date end_date, @RequestParam int people, @RequestParam int fee, @RequestParam int holiday) throws Exception{
+		int unum = 1; // 임시, 로그인기능 완성되면 수정
+		int num = 0;
+		
+		long miliseconds = System.currentTimeMillis();
+        Date date = new Date(miliseconds);
+		
+        String filename = "c_"+unum+"_"+Integer.toString(Cservice.challengeNumber()+1)+".png";
+        
+        
+		File savePath = new File("C://Users//sksms//Desktop//포트폴리오//final//Final-Project//src//main//resources//static//images//challenge//challenge_image", filename);
+		try {
+			photo.transferTo(savePath);
+		} catch (IllegalStateException e) {
+			return "fail";
+		} catch (IOException e) {
+			return "fail";
+		}
+		
+		ChallengeDTO dto = new ChallengeDTO();
+		dto.setCategory(category);
+		dto.setPhoto(filename);
+		dto.setName(name);
+		dto.setSday(start_date.toString());
+		dto.setEday(end_date.toString());
+		dto.setHoliday(holiday);
+		dto.setFee(fee);
+		dto.setMpeople(people);
+		
+		num = Cservice.challengeAdd(dto);
+		
+		return "success";
 	}
 	
 	
