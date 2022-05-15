50f7942bfe0b65b9f4817f10ca31bf8062c088b0
Honsgseok <hhss2259@naver.com>
2022-05-16 00:25:13 +0900

    홍석_sns로그인

---
+++ src/main/java/com/ccc/controller/LoginController.java
@@ -0,0 +1,67 @@
+package com.ccc.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.ccc.config.login.auth.PrincipalDetails;
+import com.ccc.dao.UserDAO;
+import com.ccc.dto.UserDTO;
+
+@Controller //view 리턴하겠다
+public class LoginController {
+	
+	@Autowired
+	private UserDAO userDAO;
+	
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder; 
+
+	
+	
+	@GetMapping("/loginForm")
+	public String loginForm() {
+		return "loginForm";
+	}
+	
+	@GetMapping("/joinForm")
+	public String joinForm() {
+		System.out.println("why?");
+		return "joinForm";
+	}
+	
+	@PostMapping("/join")
+	public String join(UserDTO user) {
+		System.out.println(user);
+		user.setRole("ROLE_USER");
+		user.setPoint(3000);
+		
+		String rawPassword = user.getPassword();
+		String encPassword = bCryptPasswordEncoder.encode(rawPassword);
+		user.setPassword(encPassword);
+		
+		userDAO.save(user);
+		return "redirect:/loginForm";		
+	}
+
+	@Secured("ROLE_USER")
+	@GetMapping("/user")
+	public @ResponseBody String user(@AuthenticationPrincipal PrincipalDetails principalDetails) { //DI(의존성 주입)된다
+		System.out.println("principalDetails"+principalDetails.getUser());
+		return "user";
+	}
+	
+	
+	
+}
---
+++ src/main/webapp/WEB-INF/views/main.jsp
@@ -0,0 +1,30 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib prefix="sec" uri="http://www.springframework.org/security/tags" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>메인</title>
+</head>
+<body>
+<h1>메인 페이지입니다.</h1>
+<!--  -->
+
+
+<!-- 로그인하지 않은 모든 사용자(로그인 중인 사용자에게는 보이지 않음) -->
+<sec:authorize access="isAnonymous()">
+    <a href="/hifive/loginForm">로그인</a>
+</sec:authorize>
+
+<!-- 로그인 중인 사용자 -->
+<sec:authorize access="isAuthenticated()">
+<p>user: <sec:authentication property="principal.user"/></p>
+<p>사용자이름: <sec:authentication property="principal.user.username"/></p>
+<p>사용자이메일: <sec:authentication property="principal.user.email"/></p>
+<p>사용한 sns: <sec:authentication property="principal.user.provider"/></p>
+    <a href="">로그아웃</a>
+</sec:authorize>
+</body>
+</html>
