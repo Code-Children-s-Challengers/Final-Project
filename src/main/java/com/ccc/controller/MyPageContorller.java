8b71db85eac1fa7efef24bbb79fee3e388b510d7
Honsgseok <hhss2259@naver.com>
2022-06-03 14:38:54 +0900

    홍석_프로필사진업로드

--- src/main/java/com/ccc/controller/MyPageContorller.java
+++ src/main/java/com/ccc/controller/MyPageContorller.java
@@ -1,16 +1,26 @@
 package com.ccc.controller;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.ccc.config.login.auth.PrincipalDetails;
 import com.ccc.dao.UserDAO;
+import com.ccc.dto.ProfileDTO;
 import com.ccc.dto.UserDTO;
-import com.ccc.service.UserService;
 
 @Controller //view 리턴하겠다
 public class MyPageContorller {
@@ -58,6 +68,50 @@ public class MyPageContorller {
 	@GetMapping("/member/myChallenges")
 	public String myChallenges() {
 		return "member/myChallenges";
+	}
+	
+	@Secured("ROLE_USER")
+	@PostMapping("/upload")
+	public String handleFileUpload(@RequestParam("file") MultipartFile file, @AuthenticationPrincipal PrincipalDetails principalDetails) throws IOException{
+		int id = principalDetails.getUser().getId();
+		ProfileDTO originalProfileImage = userDAO.findProfileImage(id);
+		
+		ProfileDTO profileImage = new ProfileDTO();
+		profileImage.setId(id);
+		profileImage.setMimetype(file.getContentType());
+		profileImage.setOriginal_name(file.getOriginalFilename());
+		profileImage.setData(file.getBytes());
+
+		if(originalProfileImage == null) {
+			userDAO.insertProfileImage(profileImage);
+		}else {
+			userDAO.updateProfileImage(profileImage);
+		}
+
+		return "redirect:/member/myPage";
+	}
+	
+	@Secured("ROLE_USER")
+	@GetMapping("/view/{id}")
+	public ResponseEntity<byte[]> findProfileImage(@PathVariable int id){
+		ProfileDTO profileIamge = userDAO.findProfileImage(id);
+		
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Content-Type",profileIamge.getMimetype());
+		headers.add("Content-Length", String.valueOf(profileIamge.getData().length));
+		
+		return new ResponseEntity<byte[]>(profileIamge.getData(),headers, HttpStatus.OK);
+	}
+	
+	@Secured("ROLE_USER")
+	@PostMapping("/nickname/{id}")
+	public String updateNickname(@PathVariable int id, @RequestParam("nickname") String nickname){
+		Map<Integer,String> map = new HashMap<Integer,String>();
+		map.put(id, nickname);
+		userDAO.updateNickname2(map);
+		return "redirect:/member/myPage";
+
+		
 	}
 	
 }
