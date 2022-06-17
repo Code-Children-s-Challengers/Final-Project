c0f190fa1fb39815935384ad3361acf7d026d638
Honsgseok <hhss2259@naver.com>
2022-06-17 19:37:15 +0900

    홍석_닉네임변경기능수정

--- src/main/resources/static/js/myPageNickname.js
+++ src/main/resources/static/js/myPageNickname.js
@@ -1,7 +1,14 @@
 var originalNickname = $("#nickname").val();
-				
+
+$("#nickname").on("click", function(){
+	$("#nicknameChecker").val("false");
+	$(".nicknameSuccess").text("");
+});
+
 $("#nicknameOverraping").on("click",function(){
 	var nickname = $("#nickname").val();
+	$("#nicknameChecker").val("false");
+	
 	if(nickname =="" || nickname.length < 2){
 		alert("닉네임은 2자 이상 8자 이하로 설정해주세요 :");
 		$(".nicknameSuccess").text("닉네임은 2자 이상 8자 이하로 설정해주세요 :)");
@@ -11,21 +18,17 @@ $("#nicknameOverraping").on("click",function(){
 		$(".nicknameSuccess").text("사용가능한 닉네임입니다");
 		$(".nicknameSuccess").css("color", "green");
 		$("#nicknameChecker").val("true");
-		$("#nickname").attr('disabled','true');
-		$("#nicknameOverraping").attr('disabled','true');
 	}else{
 		$.ajax({
 			//url : '${pageContext.request.contextPath}/checkNickname?nickname='+ nickname,
 			url : '/hifive/checkNickname?nickname='+ nickname,
-			type : 'get',
+			type : 'post',
 			cache : false,
 			success : function(data) {
 				if (data == 0) {
 					$(".nicknameSuccess").text("사용가능한 닉네임입니다");
 					$(".nicknameSuccess").css("color", "green");
 					$("#nicknameChecker").val("true");
-					$("#nickname").attr('disabled','true');
-					$("#nicknameOverraping").attr('disabled','true');
 				} else {
 					$(".nicknameSuccess").text("사용 중인 닉네임입니다 ");
 					$(".nicknameSuccess").css("color", "red");
@@ -38,6 +41,8 @@ $("#nicknameOverraping").on("click",function(){
 		});
 	}				
 });
+
+
 
 
 $("#submit").on("click", function(){
