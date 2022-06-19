46f6cc13f8f69e6a162a72dab2ccddd08009b3c5
DevYJShin <96436971+DevYJShin@users.noreply.github.com>
2022-06-19 18:52:59 +0900

    영준_Oracle DB 연동 및 Dash를 활용한 그래프 시각화 (Ver 1.1)_20220619
    영준_Oracle DB 연동 및 Dash를 활용한 그래프 시각화 (Ver 1.1)_20220619

--- DA/challenge_functions.py
+++ DA/challenge_functions.py
@@ -9,37 +9,39 @@ import plotly.express as px
 
 
 
-###
+### 데이터 자료형 변경 및 프로젝트 시작/종료 년도 추출
 def preprocess(df):
     
     # 자료형 변경
-    df['START_DAY'] = pd.to_datetime(df['START_DAY'], format="%y/%m/%d")
-    df['END_DAY'] = pd.to_datetime(df['END_DAY'], format="%y/%m/%d")
+    df['SDAY'] = pd.to_datetime(df['SDAY'], format="%Y-%m-%d")
+    df['EDAY'] = pd.to_datetime(df['EDAY'], format="%Y-%m-%d")
     
     # 년도 계산
-    df['START_YEAR'] = df['START_DAY'].dt.year
-    df['END_YEAR'] = df['END_DAY'].dt.year
+    df['START_YEAR'] = df['SDAY'].dt.year
+    df['END_YEAR'] = df['EDAY'].dt.year
     
     return df
 
 
-###
+### 사용자가 선택한 chl_nm 프로젝트에 해당하는 데이터 필터링
 def fil_ch_nm(df, chl_nm):
     
-    df = df[df['CHALLENGE_NAME'].isin(chl_nm)].copy()
+    # 데이터 필터링
+    df = df[df['NAME'].isin(chl_nm)].copy()
     
     return df
 
 
-###
+### 사용자가 선택한 year에 해당하는 데이터 필터링
 def fil_year(df, year):
     
+    # 데이터 필터링
     df = df[(df['START_YEAR']==year) | (df['END_YEAR']==year)]
     
     return df
 
 
-###
+### 사용자가 선택한 INQ_CHLG, INQ_YEAR 에 해당하는 데이터를 필터링하고 시각화 할 수 있는 데이터 형태로 만듦
 def monthly_df(df, chl_nm, year):
     
     # 챌린지에 해당하는 데이터만 필터링
@@ -49,7 +51,7 @@ def monthly_df(df, chl_nm, year):
     df = fil_year(df, year)
     
     # 실제 프로젝트 실행 한 일자 계산
-    df['PRJ_DAYS'] = df.apply(lambda x: pd.to_datetime([pj_d for pj_d in list(pd.date_range(x['START_DAY'], x['END_DAY']).strftime('%Y-%m-%d')) if pj_d not in x['DAYOFF']]) , axis=1)
+    df['PRJ_DAYS'] = df.apply(lambda x: pd.to_datetime([pj_d for pj_d in list(pd.date_range(x['SDAY'], x['EDAY']).strftime('%Y-%m-%d'))]) , axis=1)
     
     # 조회년도에 해당하는 데이터 2차 필터링
     all_prj_days = [d for d in list(itertools.chain(* df['PRJ_DAYS'])) if d.year == year]
@@ -78,20 +80,24 @@ def monthly_df(df, chl_nm, year):
 
 
 
-###
+### 월별 달성률 시각화
 def monthly_plot(df_monthly):
     
-
-    fig = px.bar(df_monthly, x='월간달성률', y='월', orientation='h', 
+    # bar plot 작성
+    fig = px.bar(df_monthly,
+                 x='월간달성률',
+                 y='월', 
+                 orientation='h', 
                  text="월간달성률",
-                 title=f"월별 달성률",
+                 title="월별 달성률",
                  width=400, height=600) 
     
+    # 배경 및 축 옵션설정
     fig.update_layout(plot_bgcolor="#EFF0F2")
     fig.update_traces(textfont_size=12, textangle=0, textposition="outside", cliponaxis=True)
     
     fig['layout']['yaxis'].title.text = ''
-    fig['layout']['xaxis'].title.text = ''
+    fig['layout']['xaxis'].title.text = '월별 달성 횟수'
     fig['layout']['yaxis']['autorange'] = "reversed"
     
     fig.update_layout(xaxis_range=[0, 100])
@@ -100,39 +106,40 @@ def monthly_plot(df_monthly):
                                    tickvals = [1,2,3,4,5,6,7,8,9,10,11,12],
                                    ticktext = ['1월','2월','3월','4월','5월','6월','7월','8월','9월','10월','11월','12월']))
 
-    fig.show()
+    return fig
     
     
-###
+### 사용자가 선택한 chl_nm, str_dt, end_dt 에 해당하는 데이터를 필터링하고 시각화 할 수 있는 데이터 형태로 만듦
 def prj_ach_df(df, chl_nm, str_dt, end_dt):
+    # 
 
     # 챌린지에 해당하는 데이터만 필터링
     df = fil_ch_nm(df, chl_nm)
     
     # 조회기간 내 프로젝트 실행 일자 계산 
-    df['PRJ_DAYS'] = df.apply(lambda x: len(pd.to_datetime([pj_d for pj_d in list(pd.date_range(x['START_DAY'], x['END_DAY'])) if ((pj_d.strftime('%Y-%m-%d') not in x['DAYOFF']) & (str_dt <= pj_d <= end_dt))])) , axis=1)
+    df['PRJ_DAYS'] = df.apply(lambda x: len(pd.to_datetime([pj_d for pj_d in list(pd.date_range(x['SDAY'], x['EDAY'])) if (str_dt <= pj_d <= end_dt)])) , axis=1)
     
     # 캠페인별 프로젝트 실행일자 계산
-    df_f = df.groupby('CHALLENGE_NAME')[['PRJ_DAYS']].count().reset_index()
+    df_f = df.groupby('NAME')[['PRJ_DAYS']].count().reset_index()
 
-    
     df_f.columns = ['프로젝트이름', '수행일자']
     
     
     return df_f
 
 
-### 
+### 프로젝트별 달성률 시각화
 def prj_ach_plot(df_prj_ach):
-    
-    fig = px.bar(df_prj_ach, x='수행일자', y='프로젝트이름', orientation='h') 
+    # 배경 및 축 옵션설정
+    fig = px.bar(df_prj_ach, x='수행일자', y='프로젝트이름', title="프로젝트별 달성률", orientation='h', width=1200, height=600) 
     fig.update_layout(plot_bgcolor="#EFF0F2")
-
+    
+    # 배경 및 축 옵션설정
     fig['layout']['yaxis'].title.text = ''
     fig['layout']['xaxis'].title.text = ''
 
     fig.update_layout(xaxis_range=[0, 100])
 
 
-    fig.show()
+    return fig
 
---
+++ DA/challenge_main.py
@@ -0,0 +1,96 @@
+# 기본적인 패키지
+import pandas as pd
+import datetime
+
+# oracle DB 접근 모듈 import
+import cx_Oracle
+
+# 한글 지원
+import os
+os.putenv('NLS_LANG', '.UTF8')
+
+# 시각화
+import dash
+# import dash_core_components as dcc
+# import dash_html_components as html
+
+from dash import dcc
+from dash import html
+
+# 시각화를 위한 패키지
+# from HIGHFIVE.challenge.challenge_functions import *
+from challenge_functions import *
+
+import warnings                   # 경고 메시지 무시?
+warnings.filterwarnings('ignore') # 경고 메시지 무시?
+
+# 환경변수 설정
+CLNT_LOC = r'C:\oraclexe\app\oracle\product\11.2.0\client' #Oracle Instant Cilent를 설치한 경로
+os.environ["PATH"] = CLNT_LOC + ";" + os.environ["PATH"] #환경변수 등록
+
+
+IP = 'localhost' #ip
+port = 1521 # 숫자
+SID = 'xe' # SID
+
+ID = 'hifive' # 유저
+PASSWORD = 'hifive' # 비밀번호
+
+
+query = """ \
+
+SELECT * from CHALLENGES
+
+"""
+
+# connection
+dsn_tns = cx_Oracle.makedsn(IP, port, SID)
+connection = cx_Oracle.connect(ID, PASSWORD, dsn_tns)
+
+# Load Data
+df = pd.read_sql(query, connection)
+# df = pd.read_csv("./CHALLENGES_20220616.csv", encoding='cp949')
+
+df = preprocess(df)
+
+# challenge
+INQ_CHLG = ['1일만보걷기도전']
+
+# 조회년도
+INQ_YEAR = 2021
+
+
+df_monthly = monthly_df(df, INQ_CHLG, INQ_YEAR)
+df_monthly.head()
+
+fig_m = monthly_plot(df_monthly)
+fig_m
+
+# challenge
+INQ_CHLG = ['카페에서텀블러사용도전', '1일만보걷기도전', '1일1커밋도전']
+
+# 조회기간
+INQ_STR_DATE = datetime.date(2018, 1, 1)   #조회시작일
+INQ_END_DATE = datetime.date(2022, 12, 16) #조회종료일
+
+# 시각화
+
+df_prj_ach = prj_ach_df(df, INQ_CHLG, INQ_STR_DATE, INQ_END_DATE)
+df_prj_ach
+
+fig_prj = prj_ach_plot(df_prj_ach)
+fig_prj
+
+# Dash App
+
+# APP 생성
+app = dash.Dash()
+
+# Layout 생성
+app.layout = html.Div([dcc.Graph(figure=fig_m, style={'display': 'inline-block'}),
+                       dcc.Graph(figure=fig_prj, style={'display': 'inline-block'})
+                       ])
+# APP 실행
+if __name__ == '__main__':
+    app.run_server()
+    app.debug = True  # Error 없으면 자동으로 서버 재시작
